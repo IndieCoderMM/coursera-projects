d320ee23b14986f69d0f2d07262d630ca9835e22
hthant <hthant00ckh@gmail.com>
2022-03-12 00:01:19 +0630

    Angular module-1 completed.

---
+++ angular-mod1/app.js
@@ -0,0 +1,41 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController ($scope) {
+	$scope.message = "";
+	$scope.lunchString = "";
+	$scope.items = 0;
+
+	$scope.checkItems = function () {
+		var lunchItems = $scope.lunchString.split(',');
+		lunchItems = removeSpaces(lunchItems);
+		$scope.items = lunchItems.length
+
+		if ($scope.items == 0 ) {
+			$scope.message = "Please enter data first!";
+		}
+		else if ($scope.items < 4) {
+			$scope.message = "Enjoy!";
+		}
+		else {
+			$scope.message = "Too Much!";
+		}
+	};
+
+	function removeSpaces (arr) {
+		console.log(arr);
+		for (var i = 0; i < arr.length; i++) {
+			if (arr[i] == null || arr[i].trim() == "") {
+				arr.splice(i, 1);
+			} 
+		}
+		return arr;
+	}
+}
+
+})();
